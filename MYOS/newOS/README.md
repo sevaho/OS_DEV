ce580c6d6cd60d494b24fed34787ff47449273eb
sevaho <sebastiaan.vanhoecke@hotmail.be>
2017-01-27 19:30:03 +0100

    erro

---
+++ MYOS/newOS/README.md
@@ -0,0 +1,5 @@
+#newOS
+
+&nbsp;
+
+problem with frame buffer
--- MYOS/newOS/src/kmain.c
+++ MYOS/newOS/src/kmain.c
@@ -1,5 +1,15 @@
+void write(unsigned int i, char c, unsigned char fg, unsigned char bg){
+  char *fb = (char *) 0x000B8010;
+  fb[i] = c;
+  fb[i+1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+}
+
+#define GREEN 2
+#define GREY  8
 int main(){
 
+  write(0,'A',GREEN,GREY);
+  return 0;
 	
-	return 0;
 }
+
